7e101104559a242080a0d84eaedf4bd54590cd62
Fanny Huang <fhuang@student.42.fr>
2016-08-22 13:02:18 +0200

    rand.sh

---
+++ rand.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+	let i=0
+	let array=(0)
+	while [ $i -lt $1 ];do
+		let rand=$RANDOM%1000
+		let array[$i]=$rand
+		echo -n ${array[$i]}
+		echo " "
+		let i+=1
+	done
+else
+	echo "usage: ./rand.sh <number>"
+	exit
+fi
