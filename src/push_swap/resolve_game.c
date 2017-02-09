a82f35bfb5bbe5fcf5c2cd4c4d805334fa4df831
Fanny HUANG <fhuang@e3r9p15.42.fr>
2017-02-09 18:09:27 +0100

    Test with reverse in progress

--- src/push_swap/resolve_game.c
+++ src/push_swap/resolve_game.c
@@ -6,14 +6,14 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/09 17:07:05 by fhuang            #+#    #+#             */
-/*   Updated: 2017/02/09 17:36:13 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/02/09 18:08:24 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 #include "libft.h"
 
-static void	finish_game(t_piles *piles)
+static void		finish_game(t_piles *piles)
 {
 	while (piles->len_b > 0)
 	{
@@ -22,15 +22,32 @@ static void	finish_game(t_piles *piles)
 	}
 }
 
-void	resolve_game(t_piles *piles)
+static _Bool	is_reverse_faster(t_piles piles, int sorted_index)
+{
+	int		i;
+
+	i = 0;
+	while (i < piles.len_a)
+	{
+		if (piles.pile_a[i] == piles.sorted_pile[sorted_index])
+			break ;
+		// if (piles.pile_a[i] < piles.pile_a[i + 1])
+			// sorted++;
+		++i;
+	}
+	return (i > piles.len_a / 2 ? true : false);
+	// return (sorted < piles.len - sorted ? false : true);
+}
+
+void			resolve_game(t_piles *piles)
 {
 	int		i;
 	_Bool	reverse;
 
 	if (!piles || !piles->pile_a || !piles->pile_b || !piles->sorted_pile)
 		return ;
-	reverse = false;
 	i = 0;
+	reverse = is_reverse_faster(*piles, i);
 	while (!is_game_finished(*piles))
 	{
 		if (is_pile_sorted(piles->pile_a, piles->len_a))
@@ -40,13 +57,15 @@ void	resolve_game(t_piles *piles)
 		}
 		if (piles->pile_a[0] != piles->sorted_pile[i])
 		{
-			rotate(&piles->pile_a, piles->len_a);
-			ft_putendl("ra");
+			// reverse ? reverse_rotate(&piles->pile_a, piles->len_a) :
+				// rotate(&piles->pile_a, piles->len_a);
+			ft_putendl(reverse ? "rra" : "ra");
 		}
 		else
 		{
 			push_to(&piles->pile_a, &piles->pile_b, &piles->len_a, &piles->len_b);
 			ft_putendl("pb");
+			reverse = is_reverse_faster(*piles, i);
 			++i;
 		}
 	}
